7e47fe9dceb4ebf6678f9fc997359904eb8a7dc2
Davinia pd <delgadavi@gmail.com>
2024-07-29 19:12:17 +0000

    test de las funciones sum, fromDollarToYen, fromEuroToDollar, fromYenToPound pasan correctamente

---
+++ .learn/resets/08-planning/app.js
@@ -0,0 +1,19 @@
+
+
+const fromEuroToDollar = (euros) => {
+    return 
+}
+
+const fromDollarToYen = (dollar) => {
+    return 
+}
+
+const fromYenToPound = (dollar) => {
+    return 
+}
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum };
---
+++ .learn/resets/11-homework/app.js
@@ -0,0 +1,37 @@
+// Esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum };
+
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+
+const fromDollarToYen = function (valueInDollar) {
+    let valueInEuro = valueInDollar / 1.07;
+    let valueInYen = valueInEuro * 156.5;
+    return valueInYen;
+}
+
+
+function fromYenToPound (valueInYen) {
+    let valueInEuro = valueInYen / 156.5;
+    let valueInPound = valueInEuro * 0.87;
+    return valueInPound;
+}
+
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound}
